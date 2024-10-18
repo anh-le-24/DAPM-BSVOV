7106a176583c3e2a46826788da6a3015f17d41b9
NganHa9797 <hahoang041104@gmail.com>
2024-10-18 15:46:30 +0700

    vsds

--- obj/Debug/net8.0/DoAnCNPM.AssemblyInfo.cs
+++ obj/Debug/net8.0/DoAnCNPM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnCNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baeeaab44d67be7133d7366494bc8de04bd91f34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be68a314774839ccfcf9cebdd18c61381979b0e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnCNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnCNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
